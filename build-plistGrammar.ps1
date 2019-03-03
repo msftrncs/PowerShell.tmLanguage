697ed1b7da38cb67d799f350b617710a08636310
msftrncs <msftrncs@midlands.net>
2019-03-03 17:03:38 -0600

    increase robustness, error control

--- build-plistGrammar.ps1
+++ build-plistGrammar.ps1
@@ -1,19 +1,30 @@
 # convertto-plist.ps1 "powershell.tmlanguage.json" >'out\PowerShellSyntax.tmLanguage'
-# create output folder if it doesn't exist
-if (-not (Test-Path 'out\')) {New-Item -type Directory 'out' >$null}
+try {
+    # create output folder if it doesn't exist
+    if (-not (Test-Path 'out\')) {New-Item -type Directory 'out' >$null}
 
-# from here on, we're converting the PowerShell.tmLanguage.JSON file to PLIST with hardcoded conversion requirements
-# start by reading in the file through ConvertFrom-JSON
-$grammar_json = Get-Content "powershell.tmlanguage.json" | ConvertFrom-Json
+    # from here on, we're converting the PowerShell.tmLanguage.JSON file to PLIST with hardcoded conversion requirements
+    # start by reading in the file through ConvertFrom-JSON
+    $grammar_json = Get-Content "powershell.tmlanguage.json" | ConvertFrom-Json
 
-# write the PList document from a custom made object, supplying some data missing from the JSON file, ignoring some JSON objects
-# and reordering the items that remain.
-[ordered]@{
-    fileTypes                                            = @('ps1', 'psm1', 'psd1')
-    $grammar_json.psobject.Properties['name'].Name       = $grammar_json.psobject.Properties['name'].Value
-    $grammar_json.psobject.Properties['patterns'].Name   = $grammar_json.psobject.Properties['patterns'].Value
-    $grammar_json.psobject.Properties['repository'].Name = $grammar_json.psobject.Properties['repository'].Value
-    $grammar_json.psobject.Properties['scopeName'].Name  = $grammar_json.psobject.Properties['scopeName'].Value
-    uuid                                                 = 'f8f5ffb0-503e-11df-9879-0800200c9a66'
-} | ConvertTo-Plist -Indent "`t" -StateEncodingAs 'UTF-8' |
-    Set-Content 'out\PowerShellSyntax.tmLanguage' -Encoding 'UTF8'
+    # create the PList grammar template, supplying some data missing from the JSON file.
+    $grammar_plist = [ordered]@{
+        fileTypes  = @('ps1', 'psm1', 'psd1')
+        uuid       = 'f8f5ffb0-503e-11df-9879-0800200c9a66'
+    }
+
+    # add to the PList grammar, only the selected items from the JSON grammar.
+    # note the keys are in reverse order from how they will appear.
+    foreach ($key in  'scopeName', 'repository', 'patterns', 'injections', 'comment', 'name') {
+        if ($grammar_json.$key) {
+            $grammar_plist.insert(1, $key, $grammar_json.$key)
+        }
+    }
+
+    # write the PList document.
+    $grammar_plist | ConvertTo-Plist -Indent "`t" -StateEncodingAs 'UTF-8' |
+        Set-Content 'out\PowerShellSyntax.tmLanguage' -Encoding 'UTF8'
+}
+catch {
+    throw # error occured, give it forward to the user
+}
