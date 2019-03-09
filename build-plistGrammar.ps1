eea630850a541a3a63d6ab39aea55d2c0d2542d4
msftrncs <msftrncs@midlands.net>
2019-03-09 02:29:49 -0600

    tweaks to if and others

--- build-plistGrammar.ps1
+++ build-plistGrammar.ps1
@@ -1,16 +1,18 @@
-# convertto-plist.ps1 "powershell.tmlanguage.json" >'out\PowerShellSyntax.tmLanguage'
+# convert the VS Code JSON PowerShell grammar to the XML PList format grammar file the EditorSyntax repository uses.
 try {
     # create output folder if it doesn't exist
-    if (-not (Test-Path 'out\')) {New-Item -type Directory 'out' >$null}
+    if (-not (Test-Path out -PathType Container)) {
+        New-Item out -ItemType Directory | Out-Null
+    }
 
     # from here on, we're converting the PowerShell.tmLanguage.JSON file to PLIST with hardcoded conversion requirements
     # start by reading in the file through ConvertFrom-JSON
-    $grammar_json = Get-Content "powershell.tmlanguage.json" | ConvertFrom-Json
+    $grammar_json = Get-Content powershell.tmlanguage.json | ConvertFrom-Json
 
     # create the PList grammar template, supplying some data missing from the JSON file.
     $grammar_plist = [ordered]@{
-        fileTypes  = @('ps1', 'psm1', 'psd1')
-        uuid       = 'f8f5ffb0-503e-11df-9879-0800200c9a66'
+        fileTypes = @('ps1', 'psm1', 'psd1')
+        uuid      = 'f8f5ffb0-503e-11df-9879-0800200c9a66'
     }
 
     # add to the PList grammar, only the selected items from the JSON grammar.
@@ -22,8 +24,8 @@ try {
     }
 
     # write the PList document.
-    $grammar_plist | ConvertTo-Plist -Indent "`t" -StateEncodingAs 'UTF-8' |
-        Set-Content 'out\PowerShellSyntax.tmLanguage' -Encoding 'UTF8'
+    $grammar_plist | ConvertTo-Plist -Indent `t -StateEncodingAs UTF-8 |
+        Set-Content out\PowerShellSyntax.tmLanguage -Encoding UTF8
 }
 catch {
     throw # error occured, give it forward to the user
